3ef5ae1234b1d963c478f964c2e469debcf835cb
HIDDIFY_plas <hoseanamir20@gmail.com>
2026-07-23 22:45:18 +0330

    Update config.js

--- config.js
+++ config.js
@@ -21,19 +21,19 @@ const CONFIG = {
                 {
                     icon: "📱",
                     name: "Hiddify",
-                    link: "#"
+                    link: "https://github.com/hiddify/hiddify-app/releases/download/v4.1.1/Hiddify-Android-universal.apk"
                 },
 
                 {
                     icon: "🚀",
                     name: "v2rayNG",
-                    link: "#"
+                    link: "https://github.com/2dust/v2rayNG/releases/download/2.2.6/v2rayNG_2.2.6-fdroid_armeabi-v7a.apk"
                 },
 
                 {
                     icon: "🦊",
                     name: "Karing",
-                    link: "#"
+                    link: "https://github.com/KaringX/karing/releases/download/v1.2.22.2502/karing_1.2.22.2502_android_arm.apk"
                 },
 
                 {
@@ -66,25 +66,25 @@ const CONFIG = {
                 {
                     icon: "💻",
                     name: "v2rayN",
-                    link: "#"
+                    link: "https://github.com/2dust/v2rayN/releases/download/7.23.4/v2rayN-windows-64-desktop.zip"
                 },
 
                 {
                     icon: "🦁",
                     name: "Hiddify",
-                    link: "#"
+                    link: "https://github.com/hiddify/hiddify-app/releases/download/v4.1.1/Hiddify-Windows-Portable-x64.zip"
                 },
 
                 {
                     icon: "🦊",
                     name: "Karing",
-                    link: "#"
+                    link: "https://github.com/KaringX/karing/releases/download/v1.2.22.2502/karing_1.2.22.2502_windows_x64.exe"
                 },
 
                 {
                     icon: "🔥",
                     name: "Happ",
-                    link: "#"
+                    link: "https://github.com/Happ-proxy/happ-desktop/releases/download/3.3.6/setup-Happ.x64.exe"
                 }
 
             ]
@@ -99,37 +99,37 @@ const CONFIG = {
                 {
                     icon: "📦",
                     name: "V2Box",
-                    link: "#"
+                    link: "https://apps.apple.com/us/app/v2box-v2ray-client/id6446814690"
                 },
 
                 {
                     icon: "🦁",
                     name: "Hiddify",
-                    link: "#"
+                    link: "https://apps.apple.com/us/app/hiddify-proxy-vpn/id6596777532"
                 },
 
                 {
                     icon: "🦊",
                     name: "Karing",
-                    link: "#"
+                    link: "https://apps.apple.com/us/app/karing/id6472431552"
                 },
 
                 {
                     icon: "⭐",
                     name: "Streisand",
-                    link: "#"
+                    link: "https://apps.apple.com/us/app/streisand/id6450534064"
                 },
 
                 {
                     icon: "⚡",
-                    name: "NapsternetV",
-                    link: "#"
+                    name: "Npv Tunnel",
+                    link: "https://apps.apple.com/us/app/npv-tunnel/id1629465476"
                 },
 
                 {
                     icon: "🔥",
                     name: "Happ",
-                    link: "#"
+                    link: "https://apps.apple.com/us/app/happ-proxy-utility/id6504287215"
                 }
 
             ]
