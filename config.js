0a596209020c689f2e0209e8434780e05f2488b5
HIDDIFY_plas <hoseanamir20@gmail.com>
2026-07-24 05:53:41 +0330

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
     categories: [
 
         {
-            title: "<img src='https://raw.githubusercontent.com/HIDDIFYplas/Hiddify_plas/main/Android.png' style='width:24px;height:24px;vertical-align:middle;margin-right:8px;'> Android",
+            title: "<img src='https://raw.githubusercontent.com/HIDDIFYplas/Hiddify_plas/main/Android.png' style='width:36px;height:36px;vertical-align:middle;margin-right:8px;'> Android",
     
             apps: [
 
